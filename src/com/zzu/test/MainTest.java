f46b47ce543b775381f278c672580c91557f2628
linyuzui <1952522719@qq.com>
2022-11-28 12:53:57 +0800

    second commit

--- src/com/zzu/test/MainTest.java
+++ src/com/zzu/test/MainTest.java
@@ -3,5 +3,6 @@ package com.zzu.test;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("it's first version");
+        System.out.println("second commit");
     }
 }
