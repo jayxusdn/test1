103770b05098f45260821d31daf27e2d4de43a49
linyuzui <1952522719@qq.com>
2022-11-28 12:54:45 +0800

    final commit

--- src/com/zzu/test/MainTest.java
+++ src/com/zzu/test/MainTest.java
@@ -4,5 +4,6 @@ public class MainTest {
     public static void main(String[] args) {
         System.out.println("it's first version");
         System.out.println("second commit");
+        System.out.println("final version");
     }
 }
